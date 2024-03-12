74ee5766fe76608591bf4c8aac23dadb7141f848
Sam Flint <60146240+s-flint-defra@users.noreply.github.com>
2024-03-12 12:28:22 +0000

    Update iceberg.sh

--- src/iceberg.sh
+++ src/iceberg.sh
@@ -2,6 +2,6 @@
 # spark.sql.catalog.spark_catalog org.apache.iceberg.spark.SparkCatalog
 # spark.sql.catalog.spark_catalog.type hadoop
 # spark.sql.catalog.spark_catalog.warehouse /iceberg_test/
-DIR=/databricks/jars
+DIR=/dbfs/databricks/jars
 
 wget --no-check-certificate -NP $DIR "https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-spark-3.4_2.12/1.4.3/iceberg-spark-3.4_2.12-1.4.3.jar"
