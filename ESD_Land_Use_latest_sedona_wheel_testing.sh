acd5a55cbcebd2ca2ec4da412a5965be5bd724bf
lfakesDEFRA <luke.fakes@defra.gov.uk>
2025-12-16 17:49:06 +0000

    created init script for land use job compute automation

---
+++ ESD_Land_Use_latest_sedona_wheel_testing.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+echo $DB_CLUSTER_NAME
+URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/esd-init-scripts/main/src
+
+wget $URL/rootcert.sh -O- | bash -ex
+wget $URL/update.sh -O- | bash -ex
+wget $URL/c_dependencies.sh -O- | bash -ex
+wget $URL/sedona_180.sh -O- | bash -ex
+
+pip install /Volumes/prd_dash_lab/esa_unrestricted/land_use_model/git_actions_testing/staging/sds_land_use_model-0.1.0-py3-none-any.whl
+
+# other libraries
+pip install pyogrio==0.7.2 
+pip install pandana==0.7 
+pip install shapely==2.0.2 
+pip install matplotlib==3.8.3 
+pip install mapclassify==2.6.1 
+pip install folium==0.15.1
