e1c7f1c5fc3bc0ccba3b43538c7c4fe380494e2a
lfakesDEFRA <luke.fakes@defra.gov.uk>
2025-12-18 13:55:42 +0000

    edited land use wheel automation script to not use the latest sedona and to install PyYAML

---
+++ ESD_Land_Use_wheel_automation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+echo $DB_CLUSTER_NAME
+URL=https://raw.githubusercontent.com/Defra-Data-Science-Centre-of-Excellence/esd-init-scripts/main/src
+
+wget $URL/rootcerts.sh -O- | bash
+wget $URL/update.sh -O- | bash 
+wget $URL/c_dependencies.sh -O- | bash 
+wget $URL/sedona_150.sh -O- | bash 
+
+pip install PyYAML
+# pip install /Workspace/Shared/python_libraries/sds_dash_download-0.1.0-py3-none-any.whl /Workspace/Shared/python_libraries/sds_land_use_model-0.1.0-py3-none-any.whl
